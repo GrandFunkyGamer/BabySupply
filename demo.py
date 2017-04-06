df88ccbb54edcd67e169eb61f6ea78a9af5e0bd9
Richard <rick.canon@yahoo.com>
2017-04-06 20:46:51 +0000

    added a class for for form inputs to be stored in datastore

--- demo.py
+++ demo.py
@@ -1,6 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+
+from google.appengine.ext import ndb
 
 app = Flask(__name__)
+
+
+class PotentialClient(ndb.Model):
+	email = ndb.StringProperty()
+
+
+@app.route('/notify', methods=['POST'])
+def notify():
+	email = request.form['name']
+	print email
+	# potential_client = PotentialClient(email)
+	# potential_client.put()
+
+
+	return redirect('/')
+
+
 
 @app.route("/")
 def home():
