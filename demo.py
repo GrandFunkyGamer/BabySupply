7e344f9102516fd69caa2eee9d3ac9750df1a1c8
Richard <rick.canon@yahoo.com>
2017-04-09 13:22:22 +0000

    Backend for form inputs & render template for redirecting

--- demo.py
+++ demo.py
@@ -5,25 +5,37 @@ from google.appengine.ext import ndb
 app = Flask(__name__)
 
 
+@app.route("/")
+def home():
+	return render_template("thebabyhub.html")
+
+
 class PotentialClient(ndb.Model):
 	email = ndb.StringProperty()
 
 
 @app.route('/notify', methods=['POST'])
 def notify():
-	email = request.form['name']
+	email = request.form.get('email')
 	print email
-	# potential_client = PotentialClient(email)
-	# potential_client.put()
+	potential_client = PotentialClient(email=email)
+	#or
+	#potential_client.email = email
+	potential_client.put()
 
 
-	return redirect('/')
+	return redirect('/thankyou')
+
+@app.route('/thankyou', methods=['GET'])
+def thankyou():
+    return render_template("thebabyhub2.html")
 
 
 
-@app.route("/")
-def home():
-	return render_template("thebabyhub.html")
+
+
+
+
 
 if __name__ == '__main__':
 	app.run(debug=True)		
